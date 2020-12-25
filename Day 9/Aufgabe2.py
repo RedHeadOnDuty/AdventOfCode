59a8877e8c4b5ab7c38172793f1802317fbe4f8d
RedHeadOnDuty <76099807+RedHeadOnDuty@users.noreply.github.com>
2020-12-25 02:31:55 +0100

    Add files via upload

---
+++ Day 9/Aufgabe2.py
@@ -0,0 +1,54 @@
+
+Input = open("input.txt").read().split("\n")
+
+accumulator = 0
+
+
+commands = []
+amount = []
+for line in Input[:-1]:
+    temp = line.split(" ")
+    commands.append(temp[0])
+    amount.append(temp[1])
+
+
+def test(cmd):
+    global accumulator
+    accumulator = 0
+    i = 0
+    watchList = []
+    while True:
+        if i in watchList:
+            return False
+        if i == len(commands):
+            return True
+        print(cmd[i], amount[i])
+        watchList.append(i)
+        if cmd[i] == "acc":
+            accumulator += int(amount[i])
+            i += 1
+        elif cmd[i] == "jmp":
+            i += int(amount[i])
+        elif cmd[i] == "nop":
+            i += 1
+
+
+def test2():
+    i = 0
+    while True:
+        cmd = commands.copy()
+        if cmd[i] == "acc":
+            i += 1
+            continue
+        elif cmd[i] == "jmp":
+            cmd[i] = "nop"
+        elif cmd == "nop":
+            cmd[i] = "jmp"
+        if test(cmd):
+            break
+        i += 1
+
+    print(accumulator)
+
+
+test2()
